71b88c7f695bc22b3db7ceb2a580b1339ccdd8f8
anantanapses <ananta.patra@napses.com>
2023-03-10 09:40:31 +0530

    Adding Heading Component

---
+++ src/components/Molecules/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+const Header = ({
+}) => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+Header.propTypes = {
+  iconSrc: PropTypes.string,
+};
+
+export default Header;
---
+++ src/components/Molecules/Header/index.stories.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Header from '../Header';
+
+export default {
+  title: 'Molecules/Headers',
+  component: Header,
+};
+
+const Template = (args) => <Header {...args} />;
+
+export const Headers = Template.bind({});
+Headers.args = {
+};
