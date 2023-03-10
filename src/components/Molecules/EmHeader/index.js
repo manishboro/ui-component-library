d72bc6c69f949396a41a2ab163f96d7cd70a7e17
anantanapses <ananta.patra@napses.com>
2023-03-10 18:55:30 +0530

    Adding Header Component

--- src/components/Molecules/EmHeader/index.js
+++ src/components/Molecules/EmHeader/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types";
 import ProfileDropDown from '../ProfileDropDown';
 import { Box, Link } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 import "./index.scss";
 
 const EmHeader = ({
@@ -17,35 +18,41 @@ const EmHeader = ({
     <Box
       bgcolor="#FCFCFC"
       display="flex"
-      justifyContent="flex-end"
+      justifyContent="space-between"
+      alignItems="center"
       borderBottom="1px solid #D1D1DB"
       py={{ xs: "14px", md: 2 }}
       px={2}
     >
-      <Box mr={1}>
-        <Link
-          component="button"
-          onClick={onClick}
-        >
-          <img src="/images/icons/call.svg" width={32} height={32} alt="call" />
-        </Link>
+      <Box style={{ cursor: "pointer" }}>
+        <MenuIcon />
       </Box>
-      <Box mr={{ xs: 3, md: 5 }}>
-        <Link
-          component="button"
-          onClick={onClick}
-        >
-          <img src="/images/icons/email.svg" width={32} height={32} alt="email" />
-        </Link>
+      <Box display="flex">
+        <Box mr={1}>
+          <Link
+            component="button"
+            onClick={onClick}
+          >
+            <img src="/images/icons/call.svg" width={32} height={32} alt="call" />
+          </Link>
+        </Box>
+        <Box mr={{ xs: 3, md: 5 }}>
+          <Link
+            component="button"
+            onClick={onClick}
+          >
+            <img src="/images/icons/email.svg" width={32} height={32} alt="email" />
+          </Link>
+        </Box>
+        <ProfileDropDown
+          avatarSrc={avatarSrc}
+          avatarAlt={avatarAlt}
+          avatarSize={avatarSize}
+          userName={userName}
+          firstLetter={firstLetter}
+          profileDropDownList={profileDropDownList}
+        />
       </Box>
-      <ProfileDropDown
-        avatarSrc={avatarSrc}
-        avatarAlt={avatarAlt}
-        avatarSize={avatarSize}
-        userName={userName}
-        firstLetter={firstLetter}
-        profileDropDownList={profileDropDownList}
-      />
     </Box>
   );
 };
