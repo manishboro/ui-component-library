7f53058c7794d1286812d8c36054a9bdb72a6f7c
anantanapses <ananta.patra@napses.com>
2023-03-15 13:51:28 +0530

    Adding Breakepoint code

--- src/components/Templates/ReviewTemplate/index.js
+++ src/components/Templates/ReviewTemplate/index.js
@@ -7,6 +7,20 @@ import EmBreadcrumb from 'components/Molecules/EmBreadcrumb'
 import EmDropDown from 'components/Molecules/EmDropDown'
 import ReviewCardGroup from 'components/Organisms/Review/ReviewCardGroup'
 import ReviewFilter from 'components/Organisms/Review/ReviewFilter'
+import { createTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from "@material-ui/styles";
+
+const theme = createTheme({
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 600,
+            md: 768,
+            lg: 1280,
+            xl: 1920,
+        },
+    },
+})
 
 const ReviewTemplate = ({
     breadcrumbs,
@@ -21,35 +35,37 @@ const ReviewTemplate = ({
     reviews
 }) => {
     return (
-        <Box>
-            <Box display="flex" alignItems={{ xs: "center", md: "flex-start" }} mb={{ xs: 1.5, md: 2.5 }}>
-                <Box width="55%">
-                    <EmBreadcrumb breadcrumbs={breadcrumbs} />
+        <ThemeProvider theme={theme}>
+            <Box>
+                <Box display="flex" alignItems={{ xs: "center", md: "flex-start" }} mb={{ xs: 1.5, md: 2.5 }}>
+                    <Box width="55%">
+                        <EmBreadcrumb breadcrumbs={breadcrumbs} />
+                    </Box>
+                    <Box width="45%" ml="auto" maxWidth="245px" pt={{ xs: 0, md: "12px" }}>
+                        <EmDropDown
+                            label="Select Year"
+                            size='large'
+                            id="reviewDropdown"
+                            labelId="reviewDropdown"
+                            optionList={yearDropdownOptions}
+                        />
+                    </Box>
                 </Box>
-                <Box width="45%" ml="auto" maxWidth="245px" pt={{ xs: 0, md: "12px" }}>
-                    <EmDropDown
-                        label="Select Year"
-                        size='large'
-                        id="reviewDropdown"
-                        labelId="reviewDropdown"
-                        optionList={yearDropdownOptions}
-                    />
-                </Box>
+
+                <ReviewCardGroup
+                    totalReview={totalReview}
+                    averageRating={averageRating}
+                    reviewRatings={reviewRatings}
+                    fbRecommendation={fbRecommendation} />
+
+                <ReviewFilter
+                    locationOptions={locationOptions}
+                    platformOptions={platformOptions}
+                    sortByOptions={sortByOptions}
+                    reviews={reviews}
+                />
             </Box>
-
-            <ReviewCardGroup
-                totalReview={totalReview}
-                averageRating={averageRating}
-                reviewRatings={reviewRatings}
-                fbRecommendation={fbRecommendation} />
-
-            <ReviewFilter
-                locationOptions={locationOptions}
-                platformOptions={platformOptions}
-                sortByOptions={sortByOptions}
-                reviews={reviews}
-            />
-        </Box>
+        </ThemeProvider>
     )
 }
 
