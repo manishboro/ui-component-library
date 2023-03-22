aa71de4c4b48965ec26291250b432f2f6704677b
robin.hooda <robin.hooda@napses.com>
2023-03-22 12:45:48 +0530

    Fix. relative path for ui-components

--- src/components/Molecules/EmPagination/index.js
+++ src/components/Molecules/EmPagination/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import { Pagination } from '@material-ui/lab';
-import "./index.scss"
+import './index.scss';
 import { Box } from '@material-ui/core';
-import EmTypography from 'components/Atoms/EmTypography';
+import EmTypography from '../../Atoms/EmTypography';
 
-const EmPagination = ({
-  className,
-  count,
-  page,
-  onChange,
-  paginationHeading
-}) => {
-
+const EmPagination = ({ className, count, page, onChange, paginationHeading }) => {
   return (
     <Box display="flex" flexDirection={{ xs: 'column', md: 'row' }} justifyContent="space-between" alignItems="center">
-      {paginationHeading && <Box marginBottom={{ xs: 2, md: 0 }} ><EmTypography variant='body2' color='textSecondary' >{paginationHeading}</EmTypography></Box>}
+      {paginationHeading && (
+        <Box marginBottom={{ xs: 2, md: 0 }}>
+          <EmTypography variant="body2" color="textSecondary">
+            {paginationHeading}
+          </EmTypography>
+        </Box>
+      )}
 
       <Pagination page={page} className={`em-pagination ${className}`} count={count} onChange={onChange} variant="outlined" shape="rounded" />
     </Box>
@@ -25,7 +24,7 @@ const EmPagination = ({
 EmPagination.propTypes = {
   className: PropTypes.string,
   count: PropTypes.number,
-  paginationHeading: PropTypes.bool
+  paginationHeading: PropTypes.bool,
 };
 
 export default EmPagination;
